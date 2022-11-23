84693cb9bf0a9903c7bafe7a50a9ad69af3326d4
Vijay Kumar <vijaykumar@Vijays-MacBook-Pro.local>
2022-11-22 19:53:22 -0500

    Adding Logic for PR Comments

--- index.js
+++ index.js
@@ -1,18 +1,74 @@
 const core = require('@actions/core');
-const wait = require('./wait');
+const github = require('@actions/github');
 
 
 // most @actions toolkit packages have async methods
 async function run() {
   try {
-    const ms = core.getInput('milliseconds');
-    core.info(`Waiting ${ms} milliseconds ...`);
+    const owner = core.getInput('owner');
+    const repo = core.getInput('repo');
+    const pr_number = core.getInput('pr_number');
+    const token = core.getInput('token');
 
-    core.debug((new Date()).toTimeString()); // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
-    await wait(parseInt(ms));
-    core.info((new Date()).toTimeString());
+    const octokit = new github.getOctokit(token);
 
-    core.setOutput('time', new Date().toTimeString());
+    const { data: changedFiles } = octokit.rest.pulls.listFiles({
+      owner,
+      repo,
+      pr_number
+    });
+
+    let diffData = {
+      addition: 0,
+      deletions: 0,
+      changes: 0
+    }
+
+    diffData = changedFiles.reduce((acc,file) => {
+      acc.addition += file.addition;
+      acc.deletions += file.deletions;
+      acc.changes += file.changes;
+      return acc;
+    },diffData)
+
+    await octokit.rest.issues.createcomment({
+      owner,
+      repo,
+      issue_number: pr_number,
+      body: `
+        Pull request #${pr_number} has be updated with : \n
+        - #${diffData.changes} changes \n
+        - #${diffData.addition} addition \n
+        - #${diffData.deletions} deletions
+      `
+    });
+
+    for(const file of changedFiles){
+      //readme.md
+      //['readme','md']
+      const fileExtension = file.filename.split('.').pop()
+      let label = ''
+      switch(fileExtension){
+        case 'md':
+          label = 'markdown'
+          break;
+        case 'js':
+          label = 'Javascript'
+          break;
+        case 'yml':
+          label = 'YAML'
+          break;
+        default:
+          label = 'noextension'
+          break;
+      }
+      await octokit.rest.issues.addLabel({
+        owner,
+        repo,
+        issue_number: pr_number,
+        labels: [label]
+      })
+    }
   } catch (error) {
     core.setFailed(error.message);
   }
